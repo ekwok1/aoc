47758a780720430d207250c69926787a26a73a00
Eugene Kwok <ekwok@relias.com>
2022-10-31 22:13:38 -0400

    2015 day 4

---
+++ 2015/04-advent-coins/b_advent-coins.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.reader import read_line
+import hashlib
+
+secretKey: str = read_line('./04.txt')
+
+i: int = 0
+while True:
+  hash: str = hashlib.md5((secretKey + str(i)).encode()).hexdigest()
+  if hash.startswith("000000"):
+    break
+  else:
+    i += 1
+
+print(i)
