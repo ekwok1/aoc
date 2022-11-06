ca707f2798c0918b66fa63b2e025754ee974ea5c
Eugene Kwok <ekwok@relias.com>
2022-11-06 08:27:21 -0500

    Use cache for day 7

--- 2015/07_assembly/a_assembly.py
+++ 2015/07_assembly/a_assembly.py
@@ -1,27 +1,22 @@
 import sys
 sys.path.insert(0, '../..')
+from functools import cache
 from utils.reader import read_lines
 from utils.string import parse_int
 
 instructions: list[str] = read_lines('./07.txt')
 
 id_inputs_dictionary: dict[str, list[str]] = dict()
-id_signal_dictionary: dict[str, int] = dict()
 
 for instruction in instructions:
   signal, identifier = instruction.split(' -> ')
-  split_signal: list[str] = signal.split(' ')
-  if len(split_signal) == 1 and parse_int(split_signal[0]) is not None:
-    id_signal_dictionary[identifier] = int(split_signal[0])
-  else:
-    id_inputs_dictionary[identifier] = split_signal
+  id_inputs_dictionary[identifier] = signal.split(' ')
 
+@cache
 def get_signal(wire_id: str) -> int:
   int_signal: int | None = parse_int(wire_id)
   if int_signal is not None:
     return int_signal
-  elif wire_id in id_signal_dictionary:
-    return id_signal_dictionary[wire_id]
   else:
     split_signal: list[str] = id_inputs_dictionary[wire_id]
     if len(split_signal) == 1:
@@ -38,7 +33,7 @@ def get_signal(wire_id: str) -> int:
         signal = get_signal(split_signal[0]) >> get_signal(split_signal[2])
       else: ## 'LSHIFT'
         signal = get_signal(split_signal[0]) << get_signal(split_signal[2])
-    id_signal_dictionary[wire_id] = signal
     return signal
 
 print(get_signal('a'))
+print(get_signal.cache_info())
--- 2015/07_assembly/b_assembly.py
+++ 2015/07_assembly/b_assembly.py
@@ -1,30 +1,23 @@
 import sys
 sys.path.insert(0, '../..')
+from functools import cache
 from utils.reader import read_lines
 from utils.string import parse_int
 
 instructions: list[str] = read_lines('./07.txt')
-override_signal: int = 956
+override_signal: list[str] = ['956']
 
 id_inputs_dictionary: dict[str, list[str]] = dict()
-id_signal_dictionary: dict[str, int] = dict()
 
 for instruction in instructions:
   signal, identifier = instruction.split(' -> ')
-  split_signal: list[str] = signal.split(' ')
-  if identifier == 'b':
-    id_signal_dictionary[identifier] = override_signal
-  elif len(split_signal) == 1 and parse_int(split_signal[0]) is not None:
-    id_signal_dictionary[identifier] = int(split_signal[0])
-  else:
-    id_inputs_dictionary[identifier] = split_signal
+  id_inputs_dictionary[identifier] = override_signal if identifier == 'b' else signal.split(' ')
 
+@cache
 def get_signal(wire_id: str) -> int:
   int_signal: int | None = parse_int(wire_id)
   if int_signal is not None:
     return int_signal
-  elif wire_id in id_signal_dictionary:
-    return id_signal_dictionary[wire_id]
   else:
     split_signal: list[str] = id_inputs_dictionary[wire_id]
     if len(split_signal) == 1:
@@ -41,7 +34,7 @@ def get_signal(wire_id: str) -> int:
         signal = get_signal(split_signal[0]) >> get_signal(split_signal[2])
       else: ## 'LSHIFT'
         signal = get_signal(split_signal[0]) << get_signal(split_signal[2])
-    id_signal_dictionary[wire_id] = signal
     return signal
 
 print(get_signal('a'))
+print(get_signal.cache_info())
