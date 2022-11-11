91a37b27276edd319b8978e29d49139e1df65f7b
Eugene Kwok <ekwok@relias.com>
2022-11-11 13:51:18 -0500

    2015 day 10 part 1 and 2

---
+++ 2015/10_look_and_say/a_look_and_say.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.reader import read_line
+
+initial_sequence: str = read_line('./10.txt')
+
+def look_and_say(sequence: str) -> str:
+  new_sequence: str = ''
+  counter: int = 1
+  for index, char in enumerate(sequence):
+    if index < len(sequence) - 1 and char == sequence[index + 1]:
+      counter += 1
+    else:
+      new_sequence += str(counter) + char
+      counter = 1
+  return new_sequence
+
+sequence: str = initial_sequence
+for _ in range(40):
+  sequence = look_and_say(sequence)
+
+print(len(sequence))
