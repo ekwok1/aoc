8dcba3cdf5a8258ebcb9c0849dcb882d1d6dcfc7
Eugene Kwok <ekwok@relias.com>
2022-11-11 12:58:57 -0500

    2015 day 9 part 1 and 2

---
+++ 2015/09_single_night/a_single_night.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(0, '../..')
+from itertools import permutations
+from utils.math import max_int
+from utils.reader import read_lines
+
+edges: list[str] = read_lines('./09.txt')
+
+locations: set[str] = set()
+distances: dict[str, dict[str, int]] = dict()
+
+for edge in edges:
+  location1, location2, distance = edge.split(' ')
+  locations.add(location1)
+  locations.add(location2)
+  distances.setdefault(location1, dict())[location2] = int(distance)
+  distances.setdefault(location2, dict())[location1] = int(distance)
+
+shortest_distance: int = max_int
+for index, permutation in enumerate(permutations(locations)):
+  total_distance: int = sum(map(lambda x, y: distances[x][y], permutation[:-1], permutation[1:]))
+  if total_distance < shortest_distance:
+    shortest_distance = total_distance
+
+print(shortest_distance)
