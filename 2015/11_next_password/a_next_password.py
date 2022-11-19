696d20334bf6dad50fc7385cc25f69aa4d5b15d4
Eugene Kwok <ekwok@relias.com>
2022-11-18 21:15:22 -0500

    2015 day 11

---
+++ 2015/11_next_password/a_next_password.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.reader import read_line
+from utils.string import has_forbidden_characters, has_pairs, has_three_sequence, increment_string
+
+initial_password: str = read_line('./11.txt')
+
+def get_next_password(password: str) -> str:
+  next_password = password
+  while True:
+    next_password: str = increment_string(next_password)
+    if has_forbidden_characters(next_password, 'iol') or not has_pairs(next_password, 2) or not has_three_sequence(next_password):
+      continue
+    return next_password
+
+print(get_next_password(initial_password))
