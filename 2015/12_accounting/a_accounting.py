651e91fa63be39f1c58799cac7edc97b635c09a0
Eugene Kwok <ekwok@relias.com>
2022-11-19 09:20:14 -0500

    2015 day 12

---
+++ 2015/12_accounting/a_accounting.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.json import parse_json, sum_ints_in_json
+from utils.reader import read_line
+
+document: str = read_line('./12.txt')
+
+print(sum_ints_in_json(parse_json(document)))
