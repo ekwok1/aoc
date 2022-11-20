8bca84701680966680f78395f242db8b934c9416
Eugene Kwok <ekwok@relias.com>
2022-11-19 19:36:27 -0500

    create directed graph dataclass

--- 2015/09_single_night/b_single_night.py
+++ 2015/09_single_night/b_single_night.py
@@ -1,25 +1,9 @@
 import sys
 sys.path.insert(0, '../..')
-from itertools import permutations
-from utils.math import max_int
+from utils.directed_graph import DirectedGraph, EdgeFormat, GraphType
 from utils.reader import read_lines
 
 edges: list[str] = read_lines('./09.txt')
 
-locations: set[str] = set()
-distances: dict[str, dict[str, int]] = dict()
-
-for edge in edges:
-  location1, location2, distance = edge.split(' ')
-  locations.add(location1)
-  locations.add(location2)
-  distances.setdefault(location1, dict())[location2] = int(distance)
-  distances.setdefault(location2, dict())[location1] = int(distance)
-
-longest_distance: int = 0
-for index, permutation in enumerate(permutations(locations)):
-  total_distance: int = sum(map(lambda x, y: distances[x][y], permutation[:-1], permutation[1:]))
-  if total_distance > longest_distance:
-    longest_distance = total_distance
-
-print(longest_distance)
+directed_graph: DirectedGraph = DirectedGraph(GraphType.TWO_WAY, EdgeFormat.WEIGHT_LAST, edges)
+print(directed_graph.get_longest_path()
---
+++ 2015/13_dinner_table/a_dinner_table.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.directed_graph import DirectedGraph, EdgeFormat, GraphType
+from utils.reader import read_lines
+
+edges: list[str] = read_lines('./13.txt')
+
+directed_graph: DirectedGraph = DirectedGraph(GraphType.ONE_WAY, EdgeFormat.WEIGHT_MIDDLE, edges)
+print(directed_graph.get_longest_round_trip(two_way=True))
