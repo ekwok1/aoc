66ce3b87b140b50b1d4582c45c8b18ea56fbd53f
Eugene Kwok <ekwok@relias.com>
2022-11-19 20:25:12 -0500

    2015 day 13 part 2

---
+++ 2015/13_dinner_table/b_dinner_table.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0, '../..')
+from utils.directed_graph import DirectedGraph, EdgeFormat, GraphType
+from utils.reader import read_lines
+
+edges: list[str] = read_lines('./13.txt')
+
+directed_graph: DirectedGraph = DirectedGraph(GraphType.ONE_WAY, EdgeFormat.WEIGHT_MIDDLE, edges)
+directed_graph.add_vertex("Me")
+for vertex in directed_graph.vertices:
+  directed_graph.add_edge("Me", vertex, 0)
+  directed_graph.add_edge(vertex, "Me", 0)
+print(directed_graph.get_longest_round_trip(two_way=True))
